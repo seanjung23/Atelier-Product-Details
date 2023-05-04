1bdb02062f80de8af7480e8c6dc0010ea28a2ef0
John Novakowski <jnovakowski797@gmail.com>
2023-05-04 19:26:56 -0400

    added review and review list components to review ratings

---
+++ client/src/components/RatingsReviews/Review.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+// John
+// Remember sync and to GIT PULL
+
+const Review = () => {
+
+  return(
+    <div>
+      <p>A review</p>
+    </div>
+  )
+};
+
+export default Review;
--- client/src/components/RatingsReviews/ReviewsList.jsx
+++ client/src/components/RatingsReviews/ReviewsList.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import Review from './Review.jsx';
 
 // John
 // Remember sync and to GIT PULL
@@ -8,6 +9,7 @@ const ReviewsList = () => {
   return(
     <div>
       <h2>Reviews List Section</h2>
+      <Review />
     </div>
   )
 };
