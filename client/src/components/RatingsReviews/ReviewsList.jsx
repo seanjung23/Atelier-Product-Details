8abfd4fab742f09a3789ad36da25a7db47c20463
John Novakowski <jnovakowski797@gmail.com>
2023-05-04 16:36:01 -0400

    initial reviews and list before changing branches

---
+++ client/src/components/RatingsReviews/ReviewsList.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+// John
+// Remember sync and to GIT PULL
+
+const ReviewsList = () => {
+
+  return(
+    <div>
+      <h2>Reviews List Section</h2>
+    </div>
+  )
+};
+
+export default ReviewsList;
