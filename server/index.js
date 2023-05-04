18889f162abe1ed60d9d9285300d54d646033a8f
Xinhuang Liu <liu.xinhuang198@gmail.com>
2023-05-04 17:54:55 -0600

    get related items half way done

--- server/index.js
+++ server/index.js
@@ -2,18 +2,23 @@ const express = require('express');
 const path = require('path');
 require('dotenv').config();
 const app = express();
-const { getProductInfo } = require('./controller');
+const { getProducts } = require('./controller');
 
 app.use(express.json());
 app.use(express.static(path.join(__dirname, '../client/dist')));
 
 
 app.get('/products/:id', (req, res) => {
-    getProductInfo(req, res, (data) => {
+    getProducts(req, res, (data) => {
       res.json(data);
     });
 });
 
+app.get('products/:id/related', (req, res) => {
+  getProducts(req, res, (data) => {
+    res.json(data);
+  });
+})
 let port = process.env.SERVER_PORT;
 
 app.listen(port, function() {
