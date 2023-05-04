d811df10f51cde1335db5e8315a2d12b921efbbf
Xinhuang Liu <liu.xinhuang198@gmail.com>
2023-05-03 18:06:51 -0600

    initial enviornment set up

---
+++ client/src/index.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import { render } from 'react-dom';
+
+render(
+  <div>
+    <p>hello world</p>
+  </div>,
+  document.getElementById("root")
+);
---
+++ server/index.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const path = require('path');
+let app = express();
+
+app.use(express.static(path.join(__dirname, '../client/dist')));
+app.use(express.json());
+
+let port = 3001;
+
+app.listen(port, function() {
+  console.log(`listening on port ${port}`);
+});
