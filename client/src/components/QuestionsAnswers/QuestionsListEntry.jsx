4df4fa20e043edef977e3a55bee04ed1947cf4ec
Sean Jung <sean.jung23@gmail.com>
2023-05-05 14:58:52 -0400

    finished react framework for Questions/Answers Section

--- client/src/components/QuestionsAnswers/QuestionsListEntry.jsx
+++ client/src/components/QuestionsAnswers/QuestionsListEntry.jsx
@@ -3,7 +3,9 @@ import React from 'react';
 const QuestionsListEntry = () => {
 
   return (
-    <div>Questions/Answers List Entry Adddssss</div>
+    <div>
+      <h3>Questions/Answers Entry</h3>
+    </div>
   )
 };
 
