0f890a736c656cb5c60ac23fb914bcc691449abb
Xinhuang Liu <liu.xinhuang198@gmail.com>
2023-05-04 22:17:33 -0600

    better version for loading items

--- client/src/components/RelatedItemsComparison/RelatedItem.jsx
+++ client/src/components/RelatedItemsComparison/RelatedItem.jsx
@@ -1,14 +1,35 @@
 import react, {useState, useEffect} from 'react';
 import axios from 'axios';
 
-export default function RelatedItem({item}) {
+export default function RelatedItem({itemId}) {
+  const [itemInfo, setItemInfo] = useState({});
 
+  useEffect(()=>{
+
+    let url = '/products/' + itemId;
+
+    axios.get(url)
+      .then(result => {
+        setItemInfo(result.data);
+      } )
+      .catch(err => console.err(err));
+
+  },[])
+
+
+  if (itemInfo.id !== undefined) {
+    return (
+      <div>
+        <p>{itemInfo.name}</p>
+      </div>
+    )
+  }
 
   return (
     <div>
-      <p>{item.name}</p>
-
+      loading spinner (Im a GIF)
     </div>
   )
+
 
 }
--- client/src/components/RelatedItemsComparison/RelatedItemList.jsx
+++ client/src/components/RelatedItemsComparison/RelatedItemList.jsx
@@ -4,48 +4,18 @@ import RelatedItem from './RelatedItem.jsx'
 
 export default function RelatedItemList({relatedItemIdList}) {
 
-  const [relatedItemList, setRelatedItemList] = useState([]);
+  if (relatedItemIdList.length !== 0) {
 
-//watch for relatedItemIdList when it has values
-  useEffect(()=>{
-//if true, for each id, request data from api
-    if (relatedItemIdList.length !== 0) {
-      //reset the item list
-      setRelatedItemList([]);
-      //want to store data in order
-      let orderedItems = [];
-      let orderedItemsCount = 0;
-      //loop the id list
-      relatedItemIdList.map((id, index) => {
-        let url = '/products/' + id;
-        axios.get(url)
-          .then(result => {
-            //when data stored, counter +1
-            orderedItems[index] = result.data;
-            orderedItemsCount++;
-            //when all data has been store successfully, set the item list
-            if (orderedItemsCount === relatedItemIdList.length) {
-              setRelatedItemList(orderedItems);
-            }
-
-          } )
-          .catch(err => console.err(err));
-      })
-    }
-  }, [relatedItemIdList])
-
-  //conditional rendering,
-  //show the item cards when all the item get from the request
-  if (relatedItemList.length !== 0) {
     return( <div>
     {
-      relatedItemList.map((item, index) => <RelatedItem key={index} item={item}/>)
+      relatedItemIdList.map((itemId, index) => <RelatedItem key={index} itemId={itemId}/>)
     }
     </div>)
-  } else {
-    //if not, put a spinner gif
-    return (<div>
-      <p>loading.....spinner (I am a GIF)</p>
-      </div>)
   }
+
+  return (<div>
+    <p>loading.....spinner (I am a GIF)</p>
+    </div>)
+
+
 }
