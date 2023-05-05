7938b37dae80d4d3bd08e437e87c21ee20c81a11
Xinhuang Liu <liu.xinhuang198@gmail.com>
2023-05-05 17:23:45 -0600

    Co-authored-by: John Novakowski <jnovak79@users.noreply.github.com> Co-authored-by: Sean Jung <seanjung23@users.noreply.github.com>

--- client/src/components/RelatedItemsComparison/RelatedItem.jsx
+++ client/src/components/RelatedItemsComparison/RelatedItem.jsx
@@ -4,6 +4,7 @@ import axios from 'axios';
 export default function RelatedItem({itemId}) {
   const [itemInfo, setItemInfo] = useState({});
   const [defaultStyle, setDefaultStyle] = useState({});
+  const [defaultImgSrc, setDefaultImgSrc] = useState('');
   const [itemRating, setItemRating] = useState(0);
   const [itemReady, setItemReady] = useState([]);
 
@@ -29,12 +30,13 @@ export default function RelatedItem({itemId}) {
         if(result.data.results.length === 1) {
 
           setDefaultStyle(result.data.results[0]);
-
+          setDefaultImgSrc(result.data.results[0].photos[0].url)
         } else {
 
           for (var style of result.data.results) {
             if (style['default?']) {
               setDefaultStyle(style);
+              setDefaultImgSrc(style.photos[0].url)
 
               break;
             }
@@ -58,7 +60,7 @@ export default function RelatedItem({itemId}) {
           avgRating += i * result.data.ratings[i];
           count += parseInt(result.data.ratings[i]);
         }
-        console.log(count);
+
         avgRating /= count;
         //for visual rating stars
         avgRating = Math.floor(4 * avgRating) /4;
@@ -67,8 +69,6 @@ export default function RelatedItem({itemId}) {
         setItemReady(itemReady => [...itemReady, result.data]);
       } )
       .catch(err => console.log(err));
-
-
 
 
   },[]);
@@ -80,12 +80,14 @@ export default function RelatedItem({itemId}) {
 
   if (itemReady.length === 3) {
     return (
-      <div>
+      <div style={({display: "inline-block"})}>
+        <svg url="./star-half.svg"></svg>
+        <img src={defaultImgSrc} style={({width: "100px", height: "100px"})}></img>
         <p>{itemInfo.name}</p>
         <p>{itemInfo.category}</p>
 
-        {defaultStyle.sale_price ? (<p> {defaultStyle.sale_price} <s>defaultStyle.original_price</s> </p>): (<p>{defaultStyle.original_price}</p>)}
-      <p>{itemRating}</p>
+      {defaultStyle.sale_price ? (<p> {defaultStyle.sale_price} <s>defaultStyle.original_price</s> </p>): (<p>{defaultStyle.original_price}</p>)}
+      {itemRating !== 0? (<p>{itemRating}</p>) : <p></p>}
       </div>
     )
   }
