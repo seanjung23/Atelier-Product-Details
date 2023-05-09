f98c36e77f53e5d69bdb4a1c2c7f2af64de4ea64
John Novakowski <jnovakowski797@gmail.com>
2023-05-09 17:45:51 -0400

    implementation of the first JEST test

--- spec/ReviewsList.test.js
+++ spec/ReviewsList.test.js
@@ -1,10 +1,10 @@
 import renderer from 'react-test-renderer';
 import ReviewsList from '../client/src/components/RatingsReviews/ReviewsList.jsx';
 
-it ('renders two or less reviews on startup', () => {
+it ('expect the reviews list to exist', () => {
   const component = renderer.create(
     <ReviewsList />,
   )
   let tree = component.toJSON();
-  expect(tree).toMatchSnapshot;
+  expect(tree).toMatchSnapshot();
 })
